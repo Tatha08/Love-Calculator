a6f8dc646867ce745e41368453d817cb93daa8d1
Tatha08 <142145835+Tatha08@users.noreply.github.com>
2023-08-13 14:57:54 +0530

    Add files via upload

---
+++ LoveCalculator.py
@@ -0,0 +1,28 @@
+#Names as input are taken fro the user
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+#Two inputs are combined in this part where number of matches are checked
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+#This part declares compatibility between 2 people
+score = int(str(first_digit) + str(second_digit))
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
